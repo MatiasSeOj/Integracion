509299340320057ef772e17ac93a7e2f6d448b28
MatiasSeOj <mnsepulvedao@alumno.iplacex.cl>
2024-01-10 18:00:21 -0300

    first commit

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+@SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
 @CucumberOptions(features="Features", glue={"StepDefinition"})
 public class Runner {
